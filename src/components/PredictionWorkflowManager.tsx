30047ee1f0022dcb5f01b851767e127c57825348
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Adds PredictionWorkflowManager component, the heart of the prediciton application.
    This component holds the state for both teams selected, the
    map selected, and gamemode selected. These component is comprised
    of the other components and utilizes callback functions to return
    the selection from its child components. This component drills the
    selections to the PredictionOutcomeHandler component which
    is then responsible for the api call and displaying predictions.

--- src/components/PredictionWorkflowManager.tsx
+++ src/components/PredictionWorkflowManager.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 import { GameMode, gameModes, Team, teams, Map, maps } from '../models';
 import { TeamSelector } from './TeamSelector';
@@ -7,11 +7,22 @@ import { GameModeSelector } from './GameModeSelector';
 import { MapSelector } from './MapSelector';
 import { PredictionOutcomeHandler } from './PredictionOutcomeHandler';
 
-const PredictionWorkFlowManager = () => {
+// import { PredictionService } from '../services';
+// import { PredictionRequestBuilder } from '../models';
+
+// // const x = new PredictionRequestBuilder(teams[0], teams[1], gameModes[0], maps[0]);
+
+// // const y = PredictionService.fetchPrediction('OpTic Texas', 'Atlanta FaZe', 'Control', 'Karachi');
+
+export const PredictionWorkFlowManager = () => {
   const [teamOneSelected, setTeamOneSelected] = useState<Team>(teams[0]);
   const [teamTwoSelected, setTeamTwoSelected] = useState<Team>(teams[1]);
   const [gameModeSelected, setGameModeSelected] = useState<GameMode>(gameModes[0]);
-  const [mapSelected, setMapSelected] = useState<Map>(maps[0]);
+  const [mapSelected, setMapSelected] = useState<Map>(gameModeSelected.maps[0]);
+
+  useEffect(() => {
+    setMapSelected(gameModeSelected.maps[0]);
+  }, [gameModeSelected]);
 
   const handleTeamOneSelection = (selectedTeam: Team) => {
     if (teamTwoSelected === selectedTeam) {
@@ -37,18 +48,43 @@ const PredictionWorkFlowManager = () => {
 
   return (
     <Container>
-      <TeamSelector onTeamSelection={handleTeamOneSelection} />
-      <Container>
-        <GameModeSelector onGameModeSelection={handleGameModeSelection} />
-        <MapSelector onMapSelection={handleMapSelection} />
-        <PredictionOutcomeHandler
-          teamOneSelected={teamOneSelected}
-          teamTwoSelected={teamTwoSelected}
-          gameModeSelected={gameModeSelected}
-          mapSelected={mapSelected}
-        />
-      </Container>
-      <TeamSelector onTeamSelection={handleTeamTwoSelection} />
+      <Row>
+        <Col xs={12} md={3}>
+          <TeamSelector
+            onTeamSelection={handleTeamOneSelection}
+            teamSelected={teamOneSelected}
+            opponentSelected={teamTwoSelected}
+            teamOptions={teams}
+          />
+        </Col>
+        {/* Middle Container - Takes up 50% of the width */}
+        <Col xs={12} md={6}>
+          <GameModeSelector
+            onGameModeSelection={handleGameModeSelection}
+            gameModeSelected={gameModeSelected}
+            gameModeOptions={gameModes}
+          />
+          <MapSelector
+            onMapSelection={handleMapSelection}
+            mapSelected={mapSelected}
+            mapOptions={gameModeSelected.maps}
+          />
+          <PredictionOutcomeHandler
+            teamOneSelected={teamOneSelected}
+            teamTwoSelected={teamTwoSelected}
+            gameModeSelected={gameModeSelected}
+            mapSelected={mapSelected}
+          />
+        </Col>
+        <Col xs={12} md={3}>
+          <TeamSelector
+            onTeamSelection={handleTeamTwoSelection}
+            teamSelected={teamTwoSelected}
+            opponentSelected={teamOneSelected}
+            teamOptions={teams}
+          />
+        </Col>
+      </Row>
     </Container>
   );
 };
