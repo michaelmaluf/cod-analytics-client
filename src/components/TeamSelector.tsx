dd649ba8d38b95aebf26e2f513f37e9af5688171
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Adds TeamSelector component which displays the 12 teams and allows the user to select team_one and team_two

--- src/components/TeamSelector.tsx
+++ src/components/TeamSelector.tsx
@@ -1,13 +1,39 @@
-import { Team } from '../models';
+import { useEffect } from 'react';
+import { Container, ListGroup, Image } from 'react-bootstrap';
+
+import { GameMode, gameModes, Team, teams, Map, maps } from '../models';
+import '../assets/styles/team-selector.css';
 
 interface TeamSelectorProps {
   onTeamSelection: (selectedTeam: Team) => void;
+  teamSelected: Team;
+  opponentSelected: Team;
+  teamOptions: Team[];
 }
 
-export const TeamSelector: React.FC<TeamSelectorProps> = ({ onTeamSelection }) => {
+export const TeamSelector: React.FC<TeamSelectorProps> = ({
+  onTeamSelection,
+  teamSelected,
+  opponentSelected,
+  teamOptions,
+}) => {
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <Container className="component-base">
+      <ListGroup>
+        {teamOptions.map((team) => (
+          <ListGroup.Item
+            key={team.name}
+            action
+            active={team.name === teamSelected.name}
+            disabled={team.name === opponentSelected.name}
+            onClick={() => onTeamSelection(team)}
+            className="listing"
+          >
+            <Image className="logo-size" src={team.logo} rounded fluid />
+            {team.name}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
   );
 };
