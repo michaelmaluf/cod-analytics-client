478658075f6c87b2552adf952820150f107da42b
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Adds GameModeSelector and MapSelector components
    These components are similar to each other. They both allow the
    user to select an option via a Carousel. The selection is then returned
    to the parent component and used once the user clicks on generate prediction.

--- src/components/GameModeSelector.tsx
+++ src/components/GameModeSelector.tsx
@@ -1,11 +1,45 @@
-import { GameMode } from "../models";
+import { Carousel, Container, Image } from 'react-bootstrap';
+
+import { GameMode } from '../models';
+import '../assets/styles/carousel-styles.css'
 
 interface GameModeSelectorProps {
   onGameModeSelection: (selectedGameMode: GameMode) => void;
+  gameModeSelected: GameMode;
+  gameModeOptions: GameMode[];
 }
-export const GameModeSelector: React.FC<GameModeSelectorProps> = ({ onGameModeSelection }) => {
+export const GameModeSelector: React.FC<GameModeSelectorProps> = ({
+  onGameModeSelection,
+  gameModeSelected,
+  gameModeOptions,
+}) => {
+  const gameModeIndex = gameModeOptions.findIndex((gameMode) => gameMode == gameModeSelected);
+
+  const containerStyle = {
+    height: '25vh',
+  };
+
+  const handleGameModeSelection = (selectedIndex: number) => {
+    const gameModeSelected = gameModeOptions[selectedIndex];
+    onGameModeSelection(gameModeSelected);
+  };
 
   return (
-    
+    <Carousel
+      className="custom-carousel d-flex align-items-center justify-content-center"
+      activeIndex={gameModeIndex}
+      onSelect={handleGameModeSelection}
+      interval={null}
+      indicators={false}
+    >
+      {gameModeOptions.map((gameMode) => (
+        <Carousel.Item key={gameMode.name} className="bg-dark text-white">
+          <Image className="d-block w-100" src={gameMode.image} alt={gameMode.name} fluid />
+          <Carousel.Caption>
+            <h3 className="text-white">{gameMode.name}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
--- src/components/MapSelector.tsx
+++ src/components/MapSelector.tsx
@@ -1,11 +1,46 @@
-import { Map } from "../models";
+import { Carousel, Image } from 'react-bootstrap';
+
+import { Map } from '../models';
+import '../assets/styles/carousel-styles.css';
 
 interface MapSelectorProps {
   onMapSelection: (selectedMap: Map) => void;
+  mapSelected: Map;
+  mapOptions: Map[];
 }
 
-export const MapSelector: React.FC<MapSelectorProps> = ({ onMapSelection }) => {
+export const MapSelector: React.FC<MapSelectorProps> = ({
+  onMapSelection,
+  mapSelected,
+  mapOptions,
+}) => {
+  const mapIndex = mapOptions.findIndex((map) => map == mapSelected);
+
+  const containerStyle = {
+    height: '25vh',
+  };
+
+  const handleGameModeSelection = (selectedIndex: number) => {
+    const mapSelected = mapOptions[selectedIndex];
+    onMapSelection(mapSelected);
+  };
+
   return (
-
-  )
-}
+    <Carousel
+      className="custom-carousel d-flex align-items-center justify-content-center"
+      activeIndex={mapIndex}
+      onSelect={handleGameModeSelection}
+      interval={null}
+      indicators={false}
+    >
+      {mapOptions.map((map) => (
+        <Carousel.Item key={map.name} className="bg-dark text-white">
+          <Image className="d-block w-100" src={map.image} alt="First slide" fluid />
+          <Carousel.Caption>
+            <h3 className="text-white">{map.name}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
