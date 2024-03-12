958ba6336fee1291259656a4f58f084e67304b33
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Adds PredictionScoreboard component which is responsible for displaying the team predictions and player predictions in a table format

---
+++ src/components/PredictionScoreboard.tsx
@@ -0,0 +1,49 @@
+import { Button, Container, Row, Col, Table } from 'react-bootstrap';
+
+import { PlayerPrediction } from '../models/Prediction';
+import { Team } from '../models';
+
+interface PredictionScoreboardProps {
+  team: Team;
+  teamScore: number;
+  playerPredictions: { [key: string]: PlayerPrediction };
+}
+
+export const PredictionScoreboard: React.FC<PredictionScoreboardProps> = ({
+  team,
+  teamScore,
+  playerPredictions,
+}) => {
+  const renderPlayerPredictions = () => {
+    return Object.entries(playerPredictions).map(([playerName, stats]) => (
+      <tr key={playerName}>
+        <td>{playerName}</td>
+        <td>{stats.kills}</td>
+        <td>{stats.deaths}</td>
+        <td>{stats.damage}</td>
+        <td>{stats.objectives}</td>
+      </tr>
+    ));
+  };
+
+  return (
+    <Row>
+      <Col xs={12} style={{ marginTop: '20px' }}>
+        <h4 style={{ float: 'left' }}>{team.name}</h4>
+        <h4 style={{ float: 'right' }}>{teamScore}</h4>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Kills</th>
+              <th>Deaths</th>
+              <th>Damage</th>
+              <th>Objectives</th>
+            </tr>
+          </thead>
+          <tbody>{renderPlayerPredictions()}</tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
