26919b07e1d12f762416a1371d0c450bddfe59c6
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Fixes bug where map carousel would bug out if the gamemode gets switched

--- src/components/PredictionWorkflowManager.tsx
+++ src/components/PredictionWorkflowManager.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row, Stack } from 'react-bootstrap';
 
 import { GameMode, gameModes, Team, teams, Map, maps } from '../models';
 import { TeamSelector } from './TeamSelector';
@@ -7,22 +7,11 @@ import { GameModeSelector } from './GameModeSelector';
 import { MapSelector } from './MapSelector';
 import { PredictionOutcomeHandler } from './PredictionOutcomeHandler';
 
-// import { PredictionService } from '../services';
-// import { PredictionRequestBuilder } from '../models';
-
-// // const x = new PredictionRequestBuilder(teams[0], teams[1], gameModes[0], maps[0]);
-
-// // const y = PredictionService.fetchPrediction('OpTic Texas', 'Atlanta FaZe', 'Control', 'Karachi');
-
 export const PredictionWorkFlowManager = () => {
   const [teamOneSelected, setTeamOneSelected] = useState<Team>(teams[0]);
   const [teamTwoSelected, setTeamTwoSelected] = useState<Team>(teams[1]);
   const [gameModeSelected, setGameModeSelected] = useState<GameMode>(gameModes[0]);
   const [mapSelected, setMapSelected] = useState<Map>(gameModeSelected.maps[0]);
-
-  useEffect(() => {
-    setMapSelected(gameModeSelected.maps[0]);
-  }, [gameModeSelected]);
 
   const handleTeamOneSelection = (selectedTeam: Team) => {
     if (teamTwoSelected === selectedTeam) {
@@ -40,6 +29,9 @@ export const PredictionWorkFlowManager = () => {
 
   const handleGameModeSelection = (selectedGameMode: GameMode) => {
     setGameModeSelected(selectedGameMode);
+    if (!selectedGameMode.maps.some((map) => map.name === mapSelected.name)) {
+      setMapSelected(selectedGameMode.maps[0]);
+    }
   };
 
   const handleMapSelection = (selectedMap: Map) => {
@@ -47,9 +39,9 @@ export const PredictionWorkFlowManager = () => {
   };
 
   return (
-    <Container>
+    <Container fluid>
       <Row>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={3} className="pe-5">
           <TeamSelector
             onTeamSelection={handleTeamOneSelection}
             teamSelected={teamOneSelected}
@@ -58,25 +50,27 @@ export const PredictionWorkFlowManager = () => {
           />
         </Col>
         {/* Middle Container - Takes up 50% of the width */}
-        <Col xs={12} md={6}>
-          <GameModeSelector
-            onGameModeSelection={handleGameModeSelection}
-            gameModeSelected={gameModeSelected}
-            gameModeOptions={gameModes}
-          />
-          <MapSelector
-            onMapSelection={handleMapSelection}
-            mapSelected={mapSelected}
-            mapOptions={gameModeSelected.maps}
-          />
-          <PredictionOutcomeHandler
-            teamOneSelected={teamOneSelected}
-            teamTwoSelected={teamTwoSelected}
-            gameModeSelected={gameModeSelected}
-            mapSelected={mapSelected}
-          />
+        <Col xs={12} md={6} className="">
+          <Stack gap={3}>
+            <GameModeSelector
+              onGameModeSelection={handleGameModeSelection}
+              gameModeSelected={gameModeSelected}
+              gameModeOptions={gameModes}
+            />
+            <MapSelector
+              onMapSelection={handleMapSelection}
+              mapSelected={mapSelected}
+              mapOptions={gameModeSelected.maps}
+            />
+            <PredictionOutcomeHandler
+              teamOneSelected={teamOneSelected}
+              teamTwoSelected={teamTwoSelected}
+              gameModeSelected={gameModeSelected}
+              mapSelected={mapSelected}
+            />
+          </Stack>
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={3} className="ps-5">
           <TeamSelector
             onTeamSelection={handleTeamTwoSelection}
             teamSelected={teamTwoSelected}
