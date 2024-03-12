b9e8acef262efe2a397edc0da1ec9d456b02a871
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Adds PredictionService class which is responsible for the API call to the backend to fetch the prediction data based on the teams, map, and gamemode

---
+++ src/services/PredictionService.ts
@@ -0,0 +1,35 @@
+import { PREDICTION_URL } from './const';
+import { PredictionResults, PredictionRequest } from '../models';
+
+export const PredictionService = {
+  async fetchPrediction(predictionRequest: PredictionRequest | undefined) {
+    if (!predictionRequest) {
+      throw new Error('The prediction request is not valid.');
+    }
+    const predictionParams = new URLSearchParams({
+      team_one_name: predictionRequest.teamOne.name,
+      team_two_name: predictionRequest.teamTwo.name,
+      game_mode: predictionRequest.gameMode.name,
+      map: predictionRequest.map.name,
+    });
+
+    const predictionUrlWithParams = PREDICTION_URL + `?${predictionParams}`;
+
+    const predictionResponse = await fetch(predictionUrlWithParams, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!predictionResponse.ok) {
+      throw new Error('Prediction data is unavailable at this time.');
+    }
+
+    const predictionResults: PredictionResults = await predictionResponse.json();
+
+    return predictionResults;
+
+    console.log(predictionResponse.json());
+  },
+};
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export { PredictionService } from './PredictionService';
