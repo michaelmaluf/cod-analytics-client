9efa8a5e2c75ab2ae23555bcdb16a900995bf8cd
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Adds PredictionOutcomeHandler components.
    This component contains the generate prediction button, the
    react-query code for calling the PredictionService to fetch the prediciton,
    and the component responsible for displaying the prediction results (PredictionScoreboard).

--- src/components/PredictionOutcomeHandler.tsx
+++ src/components/PredictionOutcomeHandler.tsx
@@ -1,4 +1,11 @@
-import { GameMode, Map, Team } from "../models"
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+
+import { GameMode, Map, Team } from '../models';
+import { PredictionService } from '../services';
+import { PredictionResults, PredictionRequest, PredictionRequestGenerator } from '../models';
+import { PredictionScoreboard } from './PredictionScoreboard';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 interface PredictionOutcomeHandlerProps {
   teamOneSelected: Team;
@@ -7,10 +14,51 @@ interface PredictionOutcomeHandlerProps {
   mapSelected: Map;
 }
 
-export const PredictionOutcomeHandler: React.FC<PredictionOutcomeHandlerProps> = ({ 
-  teamOneSelected, teamTwoSelected, gameModeSelected, mapSelected 
+export const PredictionOutcomeHandler: React.FC<PredictionOutcomeHandlerProps> = ({
+  teamOneSelected,
+  teamTwoSelected,
+  gameModeSelected,
+  mapSelected,
 }) => {
-  return (
+  const [predictionRequest, setPredictionRequest] = useState<PredictionRequest | undefined>();
 
-  )
-}
+  const { data, error, isLoading, isError } = useQuery({
+    queryKey: ['fetchPrediction', predictionRequest],
+    queryFn: () => PredictionService.fetchPrediction(predictionRequest),
+    enabled: !!predictionRequest,
+  });
+
+  const generatePrediction = () => {
+    const newPredictionRequest = new PredictionRequestGenerator(
+      teamOneSelected,
+      teamTwoSelected,
+      gameModeSelected,
+      mapSelected,
+    );
+    setPredictionRequest(newPredictionRequest);
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Button variant="danger" onClick={generatePrediction}>
+          Generate Prediction
+        </Button>
+      </Row>
+      {predictionRequest && data && (
+        <>
+          <PredictionScoreboard
+            team={predictionRequest.teamOne}
+            teamScore={data.team_one_prediction}
+            playerPredictions={data.team_one_player_predictions}
+          />
+          <PredictionScoreboard
+            team={predictionRequest.teamTwo}
+            teamScore={data.team_two_prediction}
+            playerPredictions={data.team_two_player_predictions}
+          />
+        </>
+      )}
+    </Container>
+  );
+};
