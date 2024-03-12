73f6e51de138e5f84de8a77bc7de4df9b7666dc5
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:42:34 -0700

    Adds images for team logos, maps, and gamemodes

---
+++ src/assets/images/index.ts
@@ -0,0 +1,55 @@
+import ultraLogo from './team-logos/Ultra-logo.png';
+import opticLogo from './team-logos/OpTic-logo.png';
+import fazeLogo from './team-logos/FaZe-logo.png';
+import sublinersLogo from './team-logos/Subliners-logo.png';
+import surgeLogo from './team-logos/Surge-logo.png';
+import thievesLogo from './team-logos/Thieves-logo.png';
+import guerrillasLogo from './team-logos/Guerrillas-logo.png';
+import rokkrLogo from './team-logos/RØKKR-logo.png';
+import ravensLogo from './team-logos/Ravens-logo.webp';
+import hereticsLogo from './team-logos/Heretics-logo.webp';
+import breachLogo from './team-logos/Breach-logo.png';
+import legionLogo from './team-logos/Legion-logo.png';
+
+export const logos = {
+  ultraLogo,
+  opticLogo,
+  fazeLogo,
+  sublinersLogo,
+  surgeLogo,
+  thievesLogo,
+  guerrillasLogo,
+  rokkrLogo,
+  ravensLogo,
+  hereticsLogo,
+  breachLogo,
+  legionLogo,
+};
+
+import invasionMap from './maps/Invasion-map.jpeg';
+import karachiMap from './maps/Karachi-map.jpeg';
+import subBaseMap from './maps/Subbase-map.jpeg';
+import skidrowMap from './maps/Skidrow-map.webp';
+import highriseMap from './maps/Highrise-map.webp';
+import rioMap from './maps/Rio-map.jpeg';
+import terminalMap from './maps/Terminal-map.jpeg';
+
+export const mapImages = {
+  invasionMap,
+  karachiMap,
+  subBaseMap,
+  highriseMap,
+  skidrowMap,
+  rioMap,
+  terminalMap,
+};
+
+import hardpointGameMode from './game-modes/Hardpoint-gamemode.png';
+import searchAndDestroyGameMode from './game-modes/Search-and-Destroy-gamemode.png';
+import controlGameMode from './game-modes/Control-gamemode.png';
+
+export const gameModeImages = {
+  hardpointGameMode,
+  searchAndDestroyGameMode,
+  controlGameMode,
+};
