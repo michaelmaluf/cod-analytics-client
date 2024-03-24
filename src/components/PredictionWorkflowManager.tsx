2f226ca4840ed534b386ef33fafec78d7ac950f1
Michael Maluf <malufmike@gmail.com>
2024-03-23 23:58:55 -0700

    Refactors the workflow manager component which displays page 1
    Data fetching will be triggered in this component to enhance
    user experience (when user clicks on make prediciton, there is no longer
    a 200 ms delay for fetching the data). This is done via a Memo as well.
    
    Introduces React Navigation for redirecting the user to the prediction page and back.

--- src/components/PredictionWorkflowManager.tsx
+++ src/components/PredictionWorkflowManager.tsx
@@ -1,17 +1,37 @@
-import { useState, useEffect } from 'react';
-import { Button, Col, Container, Row, Stack } from 'react-bootstrap';
+import { useState, useMemo } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
-import { GameMode, gameModes, Team, teams, Map, maps } from '../models';
+import { GameMode, gameModes, Team, teams, Map, PredictionRequestGenerator } from '../models';
 import { TeamSelector } from './TeamSelector';
 import { GameModeSelector } from './GameModeSelector';
 import { MapSelector } from './MapSelector';
-import { PredictionOutcomeHandler } from './PredictionOutcomeHandler';
+import { PredictionService } from '../services';
+import '../assets/styles/utility-components.css';
+import '../assets/styles/workflow-manager.css';
 
 export const PredictionWorkFlowManager = () => {
   const [teamOneSelected, setTeamOneSelected] = useState<Team>(teams[0]);
   const [teamTwoSelected, setTeamTwoSelected] = useState<Team>(teams[1]);
   const [gameModeSelected, setGameModeSelected] = useState<GameMode>(gameModes[0]);
   const [mapSelected, setMapSelected] = useState<Map>(gameModeSelected.maps[0]);
+  const predictionRequest = useMemo(
+    () => ({
+      teamOne: teamOneSelected,
+      teamTwo: teamTwoSelected,
+      gameMode: gameModeSelected,
+      map: mapSelected,
+    }),
+    [teamOneSelected, teamTwoSelected, gameModeSelected, mapSelected],
+  );
+  const navigate = useNavigate();
+
+  const { data } = useQuery({
+    queryKey: ['fetchPrediction', predictionRequest],
+    queryFn: () => PredictionService.fetchPrediction(predictionRequest),
+    enabled: !!predictionRequest,
+  });
 
   const handleTeamOneSelection = (selectedTeam: Team) => {
     if (teamTwoSelected === selectedTeam) {
@@ -38,9 +58,24 @@ export const PredictionWorkFlowManager = () => {
     setMapSelected(selectedMap);
   };
 
+  const generatePrediction = () => {
+    const newPredictionRequest = new PredictionRequestGenerator(
+      teamOneSelected,
+      teamTwoSelected,
+      gameModeSelected,
+      mapSelected,
+    );
+    navigate('/predictions', {
+      state: {
+        data: data,
+        predictionRequest: newPredictionRequest,
+      },
+    });
+  };
+
   return (
-    <Container fluid>
-      <Row>
+    <Container className="container-base" fluid>
+      <Row className="workflow-layout">
         <Col xs={12} md={3} className="pe-5">
           <TeamSelector
             onTeamSelection={handleTeamOneSelection}
@@ -50,25 +85,20 @@ export const PredictionWorkFlowManager = () => {
           />
         </Col>
         {/* Middle Container - Takes up 50% of the width */}
-        <Col xs={12} md={6} className="">
-          <Stack gap={3}>
-            <GameModeSelector
-              onGameModeSelection={handleGameModeSelection}
-              gameModeSelected={gameModeSelected}
-              gameModeOptions={gameModes}
-            />
-            <MapSelector
-              onMapSelection={handleMapSelection}
-              mapSelected={mapSelected}
-              mapOptions={gameModeSelected.maps}
-            />
-            <PredictionOutcomeHandler
-              teamOneSelected={teamOneSelected}
-              teamTwoSelected={teamTwoSelected}
-              gameModeSelected={gameModeSelected}
-              mapSelected={mapSelected}
-            />
-          </Stack>
+        <Col xs={12} md={6} className="ps-5 pe-5 h-100 d-flex flex-column">
+          <GameModeSelector
+            onGameModeSelection={handleGameModeSelection}
+            gameModeSelected={gameModeSelected}
+            gameModeOptions={gameModes}
+          />
+          <MapSelector
+            onMapSelection={handleMapSelection}
+            mapSelected={mapSelected}
+            mapOptions={gameModeSelected.maps}
+          />
+          <Button className="custom-button" onClick={generatePrediction}>
+            Generate Prediction
+          </Button>
         </Col>
         <Col xs={12} md={3} className="ps-5">
           <TeamSelector
@@ -82,3 +112,5 @@ export const PredictionWorkFlowManager = () => {
     </Container>
   );
 };
+
+export default PredictionWorkFlowManager;
