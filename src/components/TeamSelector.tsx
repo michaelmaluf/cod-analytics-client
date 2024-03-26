b090fee0d2d047536f3b75bec600b867c6d6394a
Michael Maluf <malufmike@gmail.com>
2024-03-25 21:58:45 -0700

    Revamps Team Selector component to showcase players for each Team
    A teams players will now be displayed after a user selects a team.
    A headshot of the player as well as their ranking in each game mode will be
    viewable for the selected team.

--- src/components/TeamSelector.tsx
+++ src/components/TeamSelector.tsx
@@ -1,13 +1,17 @@
-import { Container, ListGroup, Image } from 'react-bootstrap';
+import { Container, ListGroup, Image, Row, Col, Card } from 'react-bootstrap';
 
-import { Team } from '../models';
+import { Player, Team } from '../models';
+import { useRosterInfo } from '../hooks';
+import { playerImages } from '../assets/images';
+import { getOrdinalNumber } from '../utils';
 import '../assets/styles/team-selector.css';
 
 interface TeamSelectorProps {
   onTeamSelection: (selectedTeam: Team) => void;
   teamSelected: Team;
-  opponentSelected: Team;
+  opponentSelected?: Team;
   teamOptions: Team[];
+  flipUI: boolean;
 }
 
 export const TeamSelector: React.FC<TeamSelectorProps> = ({
@@ -15,25 +19,62 @@ export const TeamSelector: React.FC<TeamSelectorProps> = ({
   teamSelected,
   opponentSelected,
   teamOptions,
+  flipUI,
 }) => {
+  const { rosterInfo } = useRosterInfo();
+
+  const selectedRoster = rosterInfo?.find((roster) => roster.name == teamSelected?.name);
   return (
-    <Container className="component-base px-lg-3 py-lg-2">
-      <ListGroup>
-        {teamOptions.map((team) => (
-          <ListGroup.Item
-            key={team.name}
-            action
-            active={team.name === teamSelected.name}
-            disabled={team.name === opponentSelected.name}
-            onClick={() => onTeamSelection(team)}
-            className="listing"
-            style={{ '--team-color': team.color } as React.CSSProperties}
-          >
-            <Image className="logo-size" src={team.logo} rounded fluid />
-            {team.name}
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+    <Container
+      className="team-selector-container px-lg-3 py-lg-3"
+      style={{ '--team-color': teamSelected.color } as React.CSSProperties}
+      fluid
+    >
+      <Row className={`${flipUI ? 'flex-row-reverse' : ''} justify-content-around`}>
+        <Col md={4} className="team-selector-col">
+          <ListGroup>
+            {teamOptions.map((team) => (
+              <ListGroup.Item
+                key={team.name}
+                action
+                active={team.name === teamSelected.name}
+                disabled={team.name === opponentSelected?.name}
+                onClick={() => onTeamSelection(team)}
+                className="listing"
+                style={{ '--team-color': team.color } as React.CSSProperties}
+              >
+                <Image className="logo-size" src={team.logo} rounded fluid />
+                {team.name}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col md={8} className="player-display-col">
+          <Row className="h-100 py-5">
+            {selectedRoster?.players.map((player: Player) => (
+              <Col key={player.name} xs={1} md={3} className="g-4">
+                <Card className="player-card">
+                  <div className="player-headshot">
+                    <div
+                      className="background-logo"
+                      style={{
+                        background: `url(${teamSelected.logo}) center center / cover no-repeat`,
+                      }}
+                    ></div>
+                    <Card.Img variant="top" src={playerImages[player.name.toLowerCase()]} />
+                  </div>
+                  <h2 className="player-name">{player.name}</h2>
+                  <div className="player-stats">
+                    <p>Hardpoint: {getOrdinalNumber(player.hardpointRank)}</p>
+                    <p>Search & Destroy: {getOrdinalNumber(player.searchAndDestroyRank)}</p>
+                    <p>Control: {getOrdinalNumber(player.controlRank)}</p>
+                  </div>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
