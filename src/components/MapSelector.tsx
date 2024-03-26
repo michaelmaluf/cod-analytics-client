66a37b38c8c5b2cd44d741e196e4bb143abd530c
Michael Maluf <malufmike@gmail.com>
2024-03-25 21:58:45 -0700

    Revamps user experience regarding map and game mode selections
    MapSetSelector component is now responsible for map / mode selections.
    GameModeSelector component has been deleted.
    MapSetSelector now allows users to pick a best of 5 mapset to query predictions.

--- src/components/MapSelector.tsx
+++ src/components/MapSelector.tsx
@@ -1,25 +1,27 @@
 import { Carousel, Container, Image } from 'react-bootstrap';
 
-import { Map } from '../models';
+import { GameMode, Map } from '../models';
 import '../assets/styles/carousel-styles.css';
 import '../assets/styles/utility-components.css';
 
 interface MapSelectorProps {
-  onMapSelection: (selectedMap: Map) => void;
+  onMapSelection: (selectedMap: Map, gameMode: GameMode) => void;
   mapSelected: Map;
   mapOptions: Map[];
+  gameMode: GameMode;
 }
 
 export const MapSelector: React.FC<MapSelectorProps> = ({
   onMapSelection,
   mapSelected,
   mapOptions,
+  gameMode,
 }) => {
   const mapIndex = mapOptions.findIndex((map) => map == mapSelected);
 
-  const handleGameModeSelection = (selectedIndex: number) => {
+  const handleMapSelection = (selectedIndex: number) => {
     const mapSelected = mapOptions[selectedIndex];
-    onMapSelection(mapSelected);
+    onMapSelection(mapSelected, gameMode);
   };
 
   return (
@@ -27,14 +29,15 @@ export const MapSelector: React.FC<MapSelectorProps> = ({
       <Carousel
         className="custom-carousel d-flex align-items-center justify-content-center"
         activeIndex={mapIndex}
-        onSelect={handleGameModeSelection}
+        onSelect={handleMapSelection}
         interval={null}
         indicators={false}
       >
         {mapOptions.map((map) => (
           <Carousel.Item key={map.name}>
-            <Image className="d-block w-100" src={map.image} alt={map.name} fluid />
+            <Image className="d-block" src={map.image} alt={map.name} />
             <div className="map-name-caption">
+              <h3>{gameMode.name}</h3>
               <h3>{map.name}</h3>
             </div>
           </Carousel.Item>
---
+++ src/components/MapSetSelector.tsx
@@ -0,0 +1,89 @@
+import { Col, Image, Nav, Row, Tab } from 'react-bootstrap';
+
+import { GameMode, Map, MapGameModePair, Team } from '../models';
+import '../assets/styles/carousel-styles.css';
+import '../assets/styles/utility-components.css';
+import { MapSelector } from './MapSelector';
+
+interface MapSetSelectorProps {
+  teamOne: Team;
+  teamTwo: Team;
+  mapSet: MapGameModePair[];
+  onPairSelection: (mapNumber: number, map: Map, gameMode: GameMode) => void;
+}
+
+export const MapSetSelector: React.FC<MapSetSelectorProps> = ({
+  onPairSelection,
+  mapSet,
+  teamOne,
+  teamTwo,
+}) => {
+  return (
+    <Tab.Container defaultActiveKey="map1">
+      <Row className="align-items-start justfiy-content-center pt-2">
+        <Col className="team-showcase-col mt-4 me-2">
+          <div
+            className="team-showcase"
+            style={{
+              border: `0.5px solid ${teamOne.color}`,
+              background: `linear-gradient(145deg, #1d0f28 0%, #2f3360 50%, ${teamOne.color} 150%)`,
+            }}
+          >
+            <h2 className="team-id">Team A</h2>
+            <Image className="logo-size" src={teamOne.logo} rounded />
+            <h2>{teamOne.name}</h2>
+          </div>
+        </Col>
+        <Col xs={5} className="map-selection-col pb-3">
+          <Tab.Content className="d-flex align-items-center">
+            {mapSet.map((mapGameModePair, index) => (
+              <Tab.Pane eventKey={`map${index + 1}`} key={index}>
+                <MapSelector
+                  onMapSelection={(mapSelected, gameMode) =>
+                    onPairSelection(index, mapSelected, gameMode)
+                  }
+                  mapSelected={mapGameModePair.map}
+                  mapOptions={mapGameModePair.gameMode.maps.filter((map) => {
+                    if (index === 2) return map;
+                    const complementIndex = (index + 3) % 6;
+                    return map.name != mapSet[complementIndex].map.name;
+                  })}
+                  gameMode={mapGameModePair.gameMode}
+                />
+              </Tab.Pane>
+            ))}
+          </Tab.Content>
+        </Col>
+        <Col className="team-showcase-col mt-4 ms-2">
+          <div
+            className="team-showcase"
+            style={{
+              border: `0.5px solid ${teamTwo.color}`,
+              background: `linear-gradient(145deg, #1d0f28 0%, #2f3360 30%, ${teamTwo.color} 150%)`,
+            }}
+          >
+            <h2 className="team-id">Team B</h2>
+            <Image className="logo-size" src={teamTwo.logo} rounded fluid />
+            <h2>{teamTwo.name}</h2>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Nav variant="pills" className="map-set-preview d-flex justify-content-center">
+            {mapSet.map((mapGameModePair, index) => (
+              <Nav.Item key={index}>
+                <Nav.Link eventKey={`map${index + 1}`}>
+                  <div className="nav-item-container">
+                    <Image className="nav-map-image" src={mapGameModePair.map.image} rounded />
+                    <div className="fw-bold">{`Map ${index + 1}`}</div>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Col>
+      </Row>
+    </Tab.Container>
+  );
+};
