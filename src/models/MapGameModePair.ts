183d2e869862e7e82b4274db3d5d400a1a62a216
Michael Maluf <malufmike@gmail.com>
2024-03-25 21:58:45 -0700

    New interface for map & game mode pairs, implements a default set of map game mode pairs that will be used as the initial state

---
+++ src/models/MapGameModePair.ts
@@ -0,0 +1,39 @@
+import { Map, GameMode, gameModes } from '.';
+
+export interface MapGameModePair {
+  map: Map;
+  gameMode: GameMode;
+}
+
+const defaultOne: MapGameModePair = {
+  map: gameModes[0].maps[0],
+  gameMode: gameModes[0],
+};
+
+const defaultTwo: MapGameModePair = {
+  map: gameModes[1].maps[1],
+  gameMode: gameModes[1],
+};
+
+const defaultThree: MapGameModePair = {
+  map: gameModes[2].maps[2],
+  gameMode: gameModes[2],
+};
+
+const defaultFour: MapGameModePair = {
+  map: gameModes[0].maps[1],
+  gameMode: gameModes[0],
+};
+
+const defaultFive: MapGameModePair = {
+  map: gameModes[1].maps[4],
+  gameMode: gameModes[1],
+};
+
+export const defaultMapSet: MapGameModePair[] = [
+  defaultOne,
+  defaultTwo,
+  defaultThree,
+  defaultFour,
+  defaultFive,
+];
